66b97bd84d16acc35057aedc8591be73d034b57b
Lucas Cavalcante <lucas_cavalcante1@hotmail.com>
2021-09-08 17:58:53 -0300

    Homepage adicionada

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,29 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET contato. */
+router.get('/contato', function(req, res, ) {
+  res.render('contato');
+});
+
+/* GET depoimentos. */
+router.get('/depoimentos', function(req, res, ) {
+  res.render('depoimentos');
+});
+
+/* GET para quem. */
+router.get('/para_quem', function(req, res, ) {
+  res.render('para_quem');
+});
+
+/* GET quem somos. */
+router.get('/quem_somos', function(req, res, ) {
+  res.render('quem_somos');
+});
+
+/* GET vantagens. */
+router.get('/vantagens', function(req, res, ) {
+  res.render('vantagens');
+});
+
 module.exports = router;
