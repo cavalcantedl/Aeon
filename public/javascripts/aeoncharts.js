ac806c4784c53447247efad821c553208560d2e8
Maciel Eustaquio <maciel.seo@gmail.com>
2021-09-09 22:07:19 -0300

    [AEON](ME) Corrige plotagem de gráfico na branch home.

--- public/javascripts/aeoncharts.js
+++ public/javascripts/aeoncharts.js
@@ -1,30 +1,24 @@
 let ctx = document.getElementById('aeonChart');
+const data = {
+    labels: ['05', '06', '07', '08', '09', '10'],
+    datasets: [
+      {
+        label: 'Dataset 1',
+        data: [21,236,32,12,35,15],
+        borderColor: 'blue',
+        backgroundColor: 'blue',
+      },
+      {
+        label: 'Dataset 2',
+        data: [55,66,132,102,5,150],
+        borderColor: 'red',
+        backgroundColor: 'red',
+      }
+    ]
+  };
 let aeonChart = new Chart(ctx, {
     type: 'line',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
+    data: data,
     options: {
         scales: {
             y: {
