25e65f55c610051f23a197268d81167768f41763
Maciel Eustaquio <maciel.seo@gmail.com>
2021-09-30 21:08:37 -0300

    [AEON](ME) inicia processo de models.

--- controllers/aeonControllers.js
+++ controllers/aeonControllers.js
@@ -1,20 +1,4 @@
-const listaClientes = [
-    {
-        "id": 1,
-        "empresa":"C&C",
-        "criada":"23-09-2021",
-    },
-    {
-        "id": 2,
-        "empresa":"Pizzaria Domino's",
-        "criada":"23-09-2021",
-    },
-    {
-        "id": 3,
-        "empresa":"Philips",
-        "criada":"30-09-2021",
-    }
-];
+const listaClientes = require('../models/listaclientes');
 
 let aeonController = {
     index: (req, res, next) => {
