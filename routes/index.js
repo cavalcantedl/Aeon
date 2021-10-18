7dd6360b4692f911f41950ebbd06aa73a593e2ed
Maciel Eustaquio <maciel.seo@gmail.com>
2021-10-18 09:20:07 -0300

    [AEON](ME) apaga projeto antigo.

--- routes/index.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const aeonController = require('../controllers/aeonControllers');
-
-router.get('/', aeonController.index); //página inicial do SITE.
-
-module.exports = router;
