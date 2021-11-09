f37e99f08038bd1435a5b889b1d334dfbaa3c820
Maciel Eustaquio <maciel.seo@gmail.com>
2021-11-09 07:22:34 -0300

    [AEON](ME) atualiza modelagem sql.

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', paginasRouter);
 
 // view sistema
-app.use('/sistema', dashboardRouter);
+app.use('/sistema/dashboard', dashboardRouter);
 
 // view usuarios
 app.use('/users', usersRouter);
