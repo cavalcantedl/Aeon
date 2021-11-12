da5f480ec7f3065d1f872f0ca781f298868bbc9c
Maciel Eustaquio <maciel.seo@gmail.com>
2021-11-11 23:20:43 -0300

    [AEON](ME) Finaliza view da pagina visao geral cliente.

---
+++ controllers/clientesController.js
@@ -0,0 +1,24 @@
+let clientesController = {
+    clientes: (req, res, next) => {
+        res.render("sistema/clientes", {
+            titulo: "Sistema de Gestão para Agências de Marketing",
+            separador: "|",
+            marca: "Aeon",
+            descricao: "Gestão descoplicada para agências de marketing.",
+            favicon: "../images/aeon-logo.png",
+            logoImagem: "../images/aeon-logo.png",
+        })
+    },
+    cadastrarClientes: (req, res, next) => {
+        res.render("sistema/cadastrarClientes", {
+            titulo: "Sistema de Gestão para Agências de Marketing",
+            separador: "|",
+            marca: "Aeon",
+            descricao: "Gestão descoplicada para agências de marketing.",
+            favicon: "../images/aeon-logo.png",
+            logoImagem: "../images/aeon-logo.png",
+        })
+    }
+}
+
+module.exports = clientesController;
---
+++ routes/clientesRouters.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let router = express.Router();
+const clientesController = require('../controllers/clientesController');
+
+/* GET Clientes. */
+router.get('/', clientesController.clientes);
+
+/* GET Cadastrar Cliente. */
+router.get('/cadastrar', clientesController.cadastrarClientes);
+// router.post('/cadastrar', clientesController.acaoCadastrarClientes);
+
+module.exports = router;
