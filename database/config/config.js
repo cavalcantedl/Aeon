420a7c4e46ce86e42af52687f7899864ef2adff9
Maciel Eustaquio <maciel.seo@gmail.com>
2021-11-21 08:14:00 -0300

    [AEON](ME) altera colunas da pagina de cliente.

--- database/config/config.js
+++ database/config/config.js
@@ -2,22 +2,22 @@ module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "database": "aeon",
+    "host": "localhost",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "database": "aeont",
+    "host": "localhost",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "database": "aeon",
+    "host": "localhost",
     "dialect": "mysql"
   }
 }
