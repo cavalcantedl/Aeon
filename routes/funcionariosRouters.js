11ebb5029cff4b4326049d9236f0cea77b7620f2
Lucas Cavalcante <87142172+cavalcantedl@users.noreply.github.com>
2021-11-24 19:04:07 -0300

    Conserta erro de validacao em funcionarios

--- routes/funcionariosRouters.js
+++ routes/funcionariosRouters.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const funcionariosController = require("../controllers/funcionariosController");
-// const uploadArquivos = require("../middlewares/enviaArquivos");
+const uploadArquivos = require("../middlewares/enviaArquivos");
 const validaCadastro = require("../middlewares/funcionariosValidacao");
 
 /* Chama o Multer */
-// const logoCliente = uploadArquivos.uploadArquivos;
+const fotoFuncionario = uploadArquivos.uploadArquivos;
 
 /* GET Funcionários. */
 router.get('/', funcionariosController.funcionarios);
 
 /* GET Cadastrar Funcionários. */
 router.get('/cadastrar', funcionariosController.cadastrarFuncionarios);
-router.post('/cadastrar', validaCadastro.validaCadastroFuncionario, funcionariosController.acaoCadastrarFuncionarios);
+router.post('/cadastrar', fotoFuncionario.single("fotoFuncionario"), validaCadastro.validaCadastroFuncionario, funcionariosController.acaoCadastrarFuncionarios);
 
 
 module.exports = router;
