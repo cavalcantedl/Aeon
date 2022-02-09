d5f0990440f84e9a2f320f733d0aff4cfbaeffc0
Maciel Eustaquio <maciel.seo@gmail.com>
2022-02-08 22:29:52 -0300

    [AEON](ME) finaliza crud, trata logotipo cliente vazio.

--- controllers/clientesController.js
+++ controllers/clientesController.js
@@ -3,10 +3,11 @@ const { validationResult } = require("express-validator");
 const moment = require("moment");
 
 
+
 let clientesController = {
     clientes: async (req, res, next) => {
         const clienteObj = await db.Cliente.findAll();
-        console.log(clienteObj);
+        // console.log(clienteObj);
 
         return res.render("sistema/clientes", {
             titulo: "Sistema de Gestão para Agências de Marketing",
@@ -39,6 +40,8 @@ let clientesController = {
             let logotipoCliente = null;
             if (req.file !== undefined) {
                 logotipoCliente = req.file.filename;
+            } else {
+                logotipoCliente = "logotipoCliente.png";
             }
 
             const clienteObj = {
@@ -65,7 +68,7 @@ let clientesController = {
             };
 
             await db.Cliente.create(clienteObj, { include: ["endereco"]});
-            res.redirect("cadastrar");
+            res.redirect("/sistema/clientes");
 
         }
         else {
@@ -103,27 +106,17 @@ let clientesController = {
         if (alertaErros.isEmpty()){
             const cliente = await db.Cliente.findByPk(req.params.id, {include: ["endereco"]});
             let { nomeFantasia, razaoSocial, logradouro, cidade, numero, complemento, bairro, estado, pais, cep, cnpj, telefoneCelular, telefoneFixo, dataEntrada, dataSaida, nomeResponsavel } = req.body;
-            console.log(req.body);
+            // console.log(req.body);
 
             if(dataSaida == ""){
                 dataSaida = null;
             }
-            console.log(dataSaida + " DataSaida");
+            
+            // console.log(dataSaida + " DataSaida");
 
             const clienteObj = {
                 nomeFantasia: nomeFantasia,
-                razaoSocial: razaoSocial,
-                endereco: 
-                    {
-                        logradouro: logradouro,
-                        numero: numero,
-                        complemento: complemento,
-                        bairro: bairro,
-                        cidade: cidade,
-                        estado: estado,
-                        pais: pais,
-                        cep: cep
-                    },
+                razaoSocial: razaoSocial,                
                 cnpj: cnpj,
                 telefoneCelular: telefoneCelular,
                 telefoneFixo: telefoneFixo,
@@ -132,16 +125,37 @@ let clientesController = {
                 nomeResponsavel: nomeResponsavel,
             };
 
-            await db.Cliente.update(clienteObj, { where: { id_cliente: req.params.id }}, { include: ["endereco"]})
+            const enderecoObj = {
+
+                        logradouro: logradouro,
+                        numero: numero,
+                        complemento: complemento,
+                        bairro: bairro,
+                        cidade: cidade,
+                        estado: estado,
+                        pais: pais,
+                        cep: cep,
+
+            }
+
+            await db.Cliente.update(clienteObj, { where: { id_cliente: req.params.id }})
+            
+            .then(async function() {
+                await db.Endereco.update(enderecoObj, { where: { id_endereco: cliente.id_endereco }})
+              })
+            .catch(function(error){
+                  console.log(error)
+              })
+        
 
             res.redirect("/sistema/clientes");
 
         }
         else {
-            console.log(alertaErros.mapped());
-            console.log(req.body);
-            console.log("Deu ruim!");
-            console.log(alertaErros);
+            // console.log(alertaErros.mapped());
+            // console.log(req.body);
+            // console.log("Deu ruim!");
+            // console.log(alertaErros);
             return res.render("sistema/editarClientes", {
                 formAction: `/sistema/clientes/editar/${req.params.id}?_method=PUT`,
                 buttonMessage: "Cadastrar",
@@ -158,7 +172,16 @@ let clientesController = {
     },
 
     excluir: async (req, res) => {
-        await db.Cliente.destroy({ where: { id_cliente: req.params.id } });
+        const cliente = await db.Cliente.findByPk(req.params.id, {include: ["endereco"]});
+        await db.Cliente.destroy({ where: { id_cliente: req.params.id } })
+
+        .then(async function() {
+            await db.Endereco.destroy( { where: { id_endereco: cliente.id_endereco }})
+          })
+        .catch(function(error){
+              console.log(error)
+          })
+
         res.redirect("/sistema/clientes");
     },
 };
